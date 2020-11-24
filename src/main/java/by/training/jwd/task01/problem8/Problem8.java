f06c6cc8180bc2b72fb05f8c25cd301f48314dbb
Eugene <evgromadko@gmail.com>
2020-11-24 16:01:17 +0300

    edit problem8 function

--- src/main/java/by/training/jwd/task01/problem8/Problem8.java
+++ src/main/java/by/training/jwd/task01/problem8/Problem8.java
@@ -5,11 +5,12 @@ public class Problem8 {
     /* Вычислить значение функции: F(x) = {-x^2+3x+9, x >= 3; 1/(x^3-6), x < 3} */
 
     public double functionValue(double x) {
-        if(x == Math.pow(6, 1.0/3))
-            throw new IllegalArgumentException("This value is outside the scope of the function domain");
+        double prohibitedArgument = Math.pow(6, 1.0/3);
+        if(x == prohibitedArgument)
+            throw new IllegalArgumentException("This argument value is outside the scope of the function domain");
 
         if (x < 3) {
-            return 1 / (Math.pow(x, 3) - 6); //zero check
+            return 1 / (Math.pow(x, 3) - 6);
         } else {
             return -1 * Math.pow(x, 2) + 3 * x + 9;
         }
